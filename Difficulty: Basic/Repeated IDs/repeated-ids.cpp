e37753928116cbad118a9f8948432766c9280d8a
Kumari Priya Sharma <101232307+Priyasharma02821@users.noreply.github.com>
2025-01-12 19:43:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated IDs/repeated-ids.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> uniqueId(vector<int>& arr) {
+        unordered_set<int> seen;
+    vector<int> result;
+
+    for (int id : arr) {
+        if (seen.find(id) == seen.end()) {
+            result.push_back(id);
+            seen.insert(id);
+        }
+    }
+
+    return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.uniqueId(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends

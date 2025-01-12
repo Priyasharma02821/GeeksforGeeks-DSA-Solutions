1b7698a659be8510b1adf0523024e7a4f93a421c
Kumari Priya Sharma <101232307+Priyasharma02821@users.noreply.github.com>
2025-01-12 19:51:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Palindromic Array/palindromic-array.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*Complete the function below*/
+
+class Solution {
+  public:
+    bool isPalinArray(vector<int> &arr) {
+        for(num: arr){
+            if(!isPalindrome(num)){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool isPalindrome(int num) {
+    int original = num;
+    int reversed = 0;
+
+    while (num > 0) {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+
+    return original == reversed;
+}
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+
+        cout << (ob.isPalinArray(arr) ? "true" : "false") << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends

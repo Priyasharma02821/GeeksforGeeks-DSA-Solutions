ddeb9d653f580640699020903265235cad304b58
Kumari Priya Sharma <101232307+Priyasharma02821@users.noreply.github.com>
2025-02-20 22:45:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Find median in a stream/find-median-in-a-stream.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<double> getMedian(vector<int> &arr) {
+        vector<double> medians;
+    priority_queue<int> maxHeap; // Max-heap to store the smaller half
+    priority_queue<int, vector<int>, greater<int>> minHeap; // Min-heap to store the larger half
+
+    for (int num : arr) {
+        if (maxHeap.empty() || num <= maxHeap.top()) {
+            maxHeap.push(num);
+        } else {
+            minHeap.push(num);
+        }
+
+        // Balance the heaps
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+
+        // Calculate the median
+        if (maxHeap.size() == minHeap.size()) {
+            medians.push_back((maxHeap.top() + minHeap.top()) / 2.0);
+        } else {
+            medians.push_back(maxHeap.top());
+        }
+    }
+
+    return medians;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        vector<double> ans = ob.getMedian(nums);
+        cout << fixed << setprecision(1);
+        for (auto &i : ans)
+            cout << i << " ";
+        cout << "\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

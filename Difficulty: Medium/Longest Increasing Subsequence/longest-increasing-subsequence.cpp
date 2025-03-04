114028851cff9a2ccb89c06667795b5697c5cc47
Kumari Priya Sharma <101232307+Priyasharma02821@users.noreply.github.com>
2025-03-04 21:16:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int lis(vector<int>& arr) {
+        // Binary search approach
+    int n = arr.size();
+    vector<int> ans;
+
+    // Initialize the answer vector with the
+    // first element of arr
+    ans.push_back(arr[0]);
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > ans.back()) {
+
+            // If the current number is greater
+            // than the last element of the answer
+            // vector, it means we have found a
+            // longer increasing subsequence.
+            // Hence, we append the current number
+            // to the answer vector.
+            ans.push_back(arr[i]);
+        }
+        else {
+
+            // If the current number is not
+            // greater than the last element of
+            // the answer vector, we perform
+            // a binary search to find the smallest
+            // element in the answer vector that
+            // is greater than or equal to the
+            // current number.
+
+            // The lower_bound function returns
+            // an iterator pointing to the first
+            // element that is not less than
+            // the current number.
+            int low = lower_bound(ans.begin(), ans.end(),
+                                  arr[i])
+                      - ans.begin();
+
+            // We update the element at the
+            // found position with the current number.
+            // By doing this, we are maintaining
+            // a sorted order in the answer vector.
+            ans[low] = arr[i];
+        }
+    }
+
+    // The length of the answer vector
+    // represents the length of the
+    // longest increasing subsequence.
+    return ans.size();
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+
+    while (t--) {
+        int n;
+        vector<int> arr;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution obj;
+        cout << obj.lis(arr) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends

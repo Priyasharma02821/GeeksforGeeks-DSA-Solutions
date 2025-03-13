2f0b74760dd0c90031bb2c211162d8d5ccc76baf
Kumari Priya Sharma <101232307+Priyasharma02821@users.noreply.github.com>
2025-03-13 22:14:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        // code here
+        int n = wt.size();
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1));
+
+    // Build table dp[][] in bottom-up manner
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= W; j++) {
+            
+            // If there is no item or the knapsack's capacity is 0
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+            else {
+                int pick = 0;
+                
+                // Pick ith item if it does not exceed the capacity of knapsack
+                if(wt[i - 1] <= j)
+                    pick = val[i - 1] + dp[i - 1][j - wt[i - 1]];
+                    
+                // Don't pick the ith item
+                int notPick = dp[i - 1][j];
+                
+                dp[i][j] = max(pick, notPick);
+            }
+        }
+    }
+    return dp[n][W];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    // Taking total test cases
+    int testCases;
+    cin >> testCases;
+    cin.ignore();
+    while (testCases--) {
+        // Reading number of items and capacity
+        int numberOfItems, capacity;
+        vector<int> weights, values;
+        string input;
+        int number;
+
+        // Read capacity and number of items
+        getline(cin, input);
+        stringstream ss(input);
+        ss >> capacity;      // The first number is the capacity
+        ss >> numberOfItems; // The second number is the number of items
+
+        // Read values
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            values.push_back(number);
+        }
+
+        // Read weights
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            weights.push_back(number);
+        }
+
+        Solution solution;
+        cout << solution.knapsack(capacity, values, weights) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
